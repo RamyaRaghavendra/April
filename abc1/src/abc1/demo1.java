19131a0d931b6176691a79fb6afc71bbdf4a17d8
Dell <Dell@DESKTOP-VIE754T>
2019-04-24 18:57:26 +0530

    commit3

--- abc1/src/abc1/demo1.java
+++ abc1/src/abc1/demo1.java
@@ -1,12 +1,14 @@
 package abc1;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class demo1 {
 
 	public static void main(String[] args) {
 		ChromeDriver driver = new ChromeDriver();
-		driver.manage().window();
+		driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
 
 	}
 
